cefe6832be465663e49317b4217cfcf10f4bf150
chindypatras <chindydewhypatras>
2021-12-28 04:01:55 -0600

    Tampil Data

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD 2020 PHP & MYSQL + Booststrap 4</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<h1 class="text-center">UAS CRUD</h1>
+<h2 class="text-center">@angganursalam</h2>
+
+
+     <!-- Awal card form -->
+     <div class="card mt-3">
+     <div class="card-header bg-primary text-white">
+        from data mahasiswa
+</div>
+<div class="card-body">
+<from method="post" action="">
+        <div class="form-group">
+            <label>Nim</label>
+            <input type="text" name="tnim" class="form-control" placeholder="input Nim anda disini!" required>
+            <label>Nama</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan nama lengkap anda disini!" required>
+            <label>jenis kelamin</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan jenis kelamin anda disini!" required>
+            <label>jurusan</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan jurusan anda disini!" required>
+            <label>prodi</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan prodi anda disini!" required>
+            <label>umur</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan umur anda disini!" required>
+            <label>angkatan</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan angkatan anda disini!" required>
+            <label>Agama</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan Agama anda disini!" required>
+            <label>TTL</label>
+            <input type="text" name="tnim" class="form-control" placeholder="Masukan Tempat, tanggal lahir anda disini!" required>
+            <label>Hobby</label>
+            <input type="text" name="tnim" class="form-control" placeholder="masukan Hobby anda disini!" required>
+<script type="text/javacript"src="js/booststrap.min.js"></script>
+</body>
+</html> 
